5a30239badaab91808a356688b1a0e497c4e5c99
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-11 09:02:40 +0700

    add message board contract

---
+++ contracts/MessageBoard.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MessageBoard {
+
+    struct Message {
+        address user;
+        string text;
+        uint256 time;
+    }
+
+    Message[] public messages;
+
+    function post(string memory _text) public {
+        messages.push(Message(msg.sender,_text,block.timestamp));
+    }
+
+    function totalMessages() public view returns(uint){
+        return messages.length;
+    }
+}
